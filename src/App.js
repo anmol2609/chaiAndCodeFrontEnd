5a11fd88d0d3622114c4279105333cfbe75cbb2f
anmol2609 <anmoldwivedi145@gmail.com>
2024-07-28 11:16:05 +0530

    react app created

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+function App(){
+    return (
+        <div>hello baa hi so</div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
