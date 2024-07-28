79113f37e3d8fa394178f327817be29c00a24a85
anmol2609 <anmoldwivedi145@gmail.com>
2024-07-28 12:46:34 +0530

    proxy added to set backend api route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
+import axios from 'axios'
 function App(){
+    const [jokes,setJokes] = useState([])
+    useEffect(()=>{
+        axios.get('/api/jokes')
+        .then((response)=>{
+            
+            setJokes(response.data)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    },[])
     return (
-        <div>hello baa hi so</div>
+        <div>
+            jokes
+            {
+                jokes.map((item,index)=>{
+                    return (
+                        <div key={index}>{item.joke}</div>     
+                    )
+                })
+            }
+        </div>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
